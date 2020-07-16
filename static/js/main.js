e78a6ca195b9d487ae8c1395c799952f83067490
Dakshika Srivastava <dakshika001@gmail.com>
2020-07-16 15:07:14 -0700

    wip client side js

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,32 @@
+const chat_form = document.getElementById('chat-form');
+const chat_messages = document.querySelector('.chat-messages');
 const socket = io();
 
+//output message to dom
+const output_message = (message) => {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+  <p class="text">${message.text}</p>`;
+  document.querySelector('.chat-messages').appendChild(div);
+};
+
+//message from server
 socket.on('message', message => {
   console.log(message);
+  output_message(message);
+  //scroll down
+  chat_messages.scrollTop = chat_messages.scrollHeight;
+});
+
+//on sending a message
+chat_form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  //getting message text
+  const msg = e.target.elements.msg.value;
+  //emitting a message to the server
+  socket.emit('chat_message', msg);
+  //clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
 });
